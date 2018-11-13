932e33ce377a8848325c8ed33891bf066115b8c6
Thalia-S <37873775+Thalia-S@users.noreply.github.com>
2018-11-13 10:33:04 -0500

    Validation
    Client-side validation works.

--- registration_form.php
+++ registration_form.php
@@ -11,26 +11,30 @@
 	        		<div class="column">
 	        			<form id="registration_form" class="ui form" action="new_account.php" method="POST">
 	        				<h4 class="ui dividing header">Registration</h4>
-	        				<div class="field">
-						    	<!--have not inserted JS code for validating input yet-->
-						    	<label>First Name</label>
-						    	<input type="text" name = "fname" id = "fname" placeholder="First Name" maxlength="20" value="<?php if(isset($_SESSION['temp_first'])){echo $_SESSION['temp_first'];unset($_SESSION['temp_first']);}?>">
+	        				<div class="ui error message"></div>
+	        				<div class="two fields">
+		        				<div class="field">
+							    	<label>First Name</label>
+							    	<input type="text" name = "fname" id = "fname" placeholder="First Name" maxlength="20" value="<?php if(isset($_SESSION['temp_first'])){echo $_SESSION['temp_first'];unset($_SESSION['temp_first']);}?>">
+								</div>
+
+								<div class="field">
+								    <label>Last Name</label>
+							      	<input type="text" name = "lname" id = "lname" placeholder="Last Name" maxlength="20" value="<?php if(isset($_SESSION['temp_last'])){echo $_SESSION['temp_last'];unset($_SESSION['temp_last']);}?>">
+							    </div>
 							</div>
 
-							<div class="field">
-							    <label>Last Name</label>
-						      	<input type="text" name = "lname" id = "lname" placeholder="Last Name" maxlength="20" value="<?php if(isset($_SESSION['temp_last'])){echo $_SESSION['temp_last'];unset($_SESSION['temp_last']);}?>">
-						    </div>
+							<div class="two fields">
+							    <div class="field">
+							      	<label>Username</label>
+							      	<input type="text" name = "uid" id = "uid" placeholder="Username" maxlength="20" value="<?php if(isset($_SESSION['temp_user'])){echo $_SESSION['temp_user'];unset($_SESSION['temp_user']);}?>">
+							    </div>
 
-						    <div class="field">
-						      	<label>Username</label>
-						      	<input type="text" name = "uid" id = "uid" placeholder="Username" maxlength="20" value="<?php if(isset($_SESSION['temp_user'])){echo $_SESSION['temp_user'];unset($_SESSION['temp_user']);}?>">
-						    </div>
-
-						    <div class="field">
-						      	<label>Password</label>
-						      	<input type="password" name = "pwd" id = "pwd" placeholder="Password" maxlength="20" value="<?php if(isset($_SESSION['temp_pass'])){echo $_SESSION['temp_pass'];unset($_SESSION['temp_pass']);}?>">
-						    </div>
+							    <div class="field">
+							      	<label>Password</label>
+							      	<input type="password" name = "pwd" id = "pwd" placeholder="Password" maxlength="20" value="<?php if(isset($_SESSION['temp_pass'])){echo $_SESSION['temp_pass'];unset($_SESSION['temp_pass']);}?>">
+							    </div>
+							</div>
 
 						    <div class="field">
 						      	<label>Email</label>
@@ -39,7 +43,7 @@
 
 						    <div class="field">
 							    <div class="ui checkbox">
-							      <input type="checkbox">
+							      <input type="checkbox" name="terms">
 							      <label>I agree to the Terms and Conditions</label>
 							    </div>
 							</div>
--- validate.js
+++ validate.js
@@ -1,16 +1,11 @@
-//Will validation work if header in another folder provides link to Semantic UI?
-
 $('.ui')
   .form({
     on: 'blur',  //turns empty fields red
+    inline: true,
     fields: {
       fname: {
         identifier: 'fname',
         rules: [
-          {
-            type   : 'empty',
-            prompt : 'Please enter your first name'
-          },
           {
             type   : 'regExp[^([a-zA-Z]\'*-* ?){1,20}$]',
             prompt : 'Please enter a valid first name'
@@ -21,10 +16,6 @@ $('.ui')
         identifier: 'lname',
         rules: [
           {
-            type   : 'empty',
-            prompt : 'Please enter your last name'
-          },
-          {
             type   : 'regExp[^([a-zA-Z]\'*-* ?){2,20}$]',
             prompt : 'Please enter a valid last name'
           }
@@ -33,10 +24,6 @@ $('.ui')
       uid: {
         identifier: 'uid',
         rules: [
-          {
-            type  : 'empty',
-            prompt  : 'Please enter your username'
-          },
           {
             type   : 'regExp[/^[a-z0-9_-]{4,16}$/]',
             prompt : 'Please enter a 4-16 letter username'
@@ -47,10 +34,6 @@ $('.ui')
         identifier: 'pwd',
         rules: [
           {
-            type  : 'empty',
-            prompt  : 'Please enter your password'
-          },
-          {
             type  : 'regExp[^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d])([^\s]){8,16}$]',
             prompt  : 'Password must contain 1 uppercase and lowercase letter, 1 number, and be 8-20 characters long.'
           }
@@ -59,10 +42,6 @@ $('.ui')
       email: {
         identifier: 'email',
         rules: [
-          {
-            type  : 'empty',
-            prompt  : 'Please enter your email'
-          },
           {
             type  : 'email',
             prompt  : 'Please enter a valid email'
