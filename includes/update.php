c092e6a9370126b2261630e25b3c3797d14a1372
Thalia-S <37873775+Thalia-S@users.noreply.github.com>
2018-11-14 20:28:33 -0500

    Admin Alter Tables
    Able to edit tables. Add and delete functions still to come.

---
+++ includes/update.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+include('db.php');
+
+  if(isset($_POST['update'])) {
+      $id = $_SESSION['temp_prod_id'];
+      $prodName = $_POST['prodName'];
+      $prodBrand = $_POST['prodBrand'];
+      $prodStock = $_POST['prodStock'];
+      $prodSale = $_POST['prodSale'];
+      $prodPrice = $_POST['prodPrice'];
+
+    //Update product in db
+    $sql = "UPDATE products
+    SET prodName = '$prodName', prodBrand = '$prodBrand', prodStock = '$prodStock', prodSale = '$prodSale', prodPrice = '$prodPrice'
+    WHERE PK = '$id'";
+    if(mysqli_query($dbc, $sql)) {
+
+      header("Location: ../admin.php?update=success");
+      //$_SESSION['user_success'] = "Profile successfully updated.";
+      unset($id);
+      exit();
+    }else {
+      header("Location: ../admin.php?update=failure");
+      $_SESSION['user_error'] = "Unable to update profile.";
+      unset($id);
+      exit();
+    }
+  }
+?>
